3c05aafb0847c6062cfe8d38f5cfaacaa7b84f26
TECNICO EM. INFORMATICA 2020.2 <51121742020.2@senacrjedu.local>
2021-08-17 09:03:42 -0300

    Atualização de fotos 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PanelcontrolComponent } from './admin/panelcontrol/panelcontrol.component';
+
 import { ContatosComponent } from './contatos/contatos.component';
 
 import { FormComponent } from './form/form.component';
 import { FotosComponent } from './fotos/fotos.component';
 import { HomeComponent } from './home/home.component';
-import { ProdutosComponent } from './produtos/produtos.component';
+
 
 
 const routes: Routes = [
@@ -19,10 +19,7 @@ const routes: Routes = [
   {
     path: 'formulario', component: FormComponent
   },
-
-  {
-    path: 'produtos', component: ProdutosComponent
-  },
+  
   {
     path: 'fotos', component: FotosComponent
   },
